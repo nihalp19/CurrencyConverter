e836a201a3c0f57af9085a140006346df1d3f1e5
nihalp19 <nihalpanday2020@gmail.com>
2024-08-24 16:19:36 +0530

    currency converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { CurrencyCodes } from "./CurrencyCodes";
+import axios from "axios";
+import { GoArrowSwitch } from "react-icons/go";
 
 function App() {
+
+  const [from, setFrom] = useState('AFN')
+  const [to, setTo] = useState('AFN')
+  const [amount,setAmount] = useState(0)
+  const [responseData,setresponseData] = useState({})
+
+
+  const style = {
+    App: 'flex flex-col  w-full items-center',
+    heading: 'text-4xl my-[32px]',
+    form: 'm-[20px] text-center',
+    selection: 'flex items-center  ',
+    from: 'mx-[10px]',
+    to: 'mx-[10px]',
+    select: 'p-[10px] bg-orange-600 text-white rounded',
+    icon: 'mx-[32px]',
+    amount: 'p-[5px] my-[20px] outline',
+    button: 'bg-blue-500 p-[5px] rounded text-white',
+    result: 'text-2xl text-green-500 mt-[20px]'
+  }
+
+  
+
+  const Convert = async () => {
+    const response = await axios.get(`https://v6.exchangerate-api.com/v6/79e445698dc6ca7775a5afe0/pair/${from}/${to}/${amount}`)
+    setresponseData(response.data)
+    console.log(responseData);
+  }
+  console.log(responseData);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.App}>
+      <h1 className={style.heading}>Currency Converter</h1>
+      <div className={style.form}>
+        <div className={style.selection}>
+          <label htmlFor="from" className={style.from} >From</label>
+          <select id="from" className={style.select} onChange={(e) => setFrom(e.target.value)}>
+            {CurrencyCodes.length > 0 && (
+              CurrencyCodes.map((c) => {
+                return <option value={c}>{c}</option>
+              })
+            )}
+          </select>
+          <GoArrowSwitch className={style.icon} />
+          <label htmlFor="to" className={style.to}>To</label>
+          <select id="to" className={style.select} onChange={(e) => setTo(e.target.value)}>
+            {CurrencyCodes.length > 0 && (
+              CurrencyCodes.map((c) => {
+                return <option value={c}>{c}</option>
+              })
+            )}
+          </select>
+        </div>
+        <input type="text" value={amount} className={style.amount} placeholder="Amount..." onChange={(e) => setAmount(e.target.value)}/>
+      </div>
+      <button className={style.button} onClick={() => Convert()}>Convert</button>
+      <p className={style.result}>{responseData === null ? " " : `${amount} ${from} equals to  ${responseData.conversion_result} ${to}`}</p>
     </div>
   );
 }
 
 export default App;
+{/*  */ }
---
+++ src/CurrencyCodes.js
@@ -0,0 +1,147 @@
+export const CurrencyCodes = [
+    'AFN', // Afghan Afghani
+    'ALL', // Albanian Lek
+    'DZD', // Algerian Dinar
+    'AOA', // Angolan Kwanza
+    'ARS', // Argentine Peso
+    'AUD', // Australian Dollar
+    'AZN', // Azerbaijani Manat
+    'BHD', // Bahraini Dinar
+    'BDT', // Bangladeshi Taka
+    'BYN', // Belarusian Ruble
+    'BZD', // Belize Dollar
+    'BMD', // Bermudian Dollar
+    'BTN', // Bhutanese Ngultrum
+    'BOB', // Bolivian Boliviano
+    'BAM', // Bosnia-Herzegovina Convertible Mark
+    'BWP', // Botswanan Pula
+    'BRL', // Brazilian Real
+    'BND', // Brunei Dollar
+    'BGN', // Bulgarian Lev
+    'BIF', // Burundian Franc
+    'KHR', // Cambodian Riel
+    'CAD', // Canadian Dollar
+    'CVE', // Cape Verdean Escudo
+    'XAF', // Central African CFA Franc
+    'CLP', // Chilean Peso
+    'CNY', // Chinese Yuan
+    'COP', // Colombian Peso
+    'KMF', // Comorian Franc
+    'CDF', // Congolese Franc
+    'CRC', // Costa Rican Colón
+    'HRK', // Croatian Kuna
+    'CUP', // Cuban Peso
+    'CZK', // Czech Koruna
+    'DKK', // Danish Krone
+    'DJF', // Djiboutian Franc
+    'DOP', // Dominican Peso
+    'EGP', // Egyptian Pound
+    'ERN', // Eritrean Nakfa
+    'ETB', // Ethiopian Birr
+    'EUR', // Euro
+    'FJD', // Fijian Dollar
+    'GMD', // Gambian Dalasi
+    'GEL', // Georgian Lari
+    'GHS', // Ghanaian Cedi
+    'GTQ', // Guatemalan Quetzal
+    'GNF', // Guinean Franc
+    'GYD', // Guyanaese Dollar
+    'HTG', // Haitian Gourde
+    'HNL', // Honduran Lempira
+    'HKD', // Hong Kong Dollar
+    'HUF', // Hungarian Forint
+    'ISK', // Icelandic Króna
+    'INR', // Indian Rupee
+    'IDR', // Indonesian Rupiah
+    'IRR', // Iranian Rial
+    'IQD', // Iraqi Dinar
+    'ILS', // Israeli New Shekel
+    'JMD', // Jamaican Dollar
+    'JPY', // Japanese Yen
+    'JOD', // Jordanian Dinar
+    'KZT', // Kazakhstani Tenge
+    'KES', // Kenyan Shilling
+    'KWD', // Kuwaiti Dinar
+    'KGS', // Kyrgystani Som
+    'LAK', // Laotian Kip
+    'LBP', // Lebanese Pound
+    'LSL', // Lesotho Loti
+    'LRD', // Liberian Dollar
+    'LYD', // Libyan Dinar
+    'MOP', // Macanese Pataca
+    'MKD', // Macedonian Denar
+    'MGA', // Malagasy Ariary
+    'MWK', // Malawian Kwacha
+    'MYR', // Malaysian Ringgit
+    'MVR', // Maldivian Rufiyaa
+    'MRU', // Mauritanian Ouguiya
+    'MUR', // Mauritian Rupee
+    'MXN', // Mexican Peso
+    'MDL', // Moldovan Leu
+    'MNT', // Mongolian Tugrik
+    'MAD', // Moroccan Dirham
+    'MZN', // Mozambican Metical
+    'MMK', // Myanmar Kyat
+    'NAD', // Namibian Dollar
+    'NPR', // Nepalese Rupee
+    'ANG', // Netherlands Antillean Guilder
+    'NZD', // New Zealand Dollar
+    'NIO', // Nicaraguan Córdoba
+    'NGN', // Nigerian Naira
+    'KPW', // North Korean Won
+    'NOK', // Norwegian Krone
+    'OMR', // Omani Rial
+    'PKR', // Pakistani Rupee
+    'PAB', // Panamanian Balboa
+    'PGK', // Papua New Guinean Kina
+    'PYG', // Paraguayan Guarani
+    'PEN', // Peruvian Sol
+    'PHP', // Philippine Peso
+    'PLN', // Polish Złoty
+    'QAR', // Qatari Rial
+    'RON', // Romanian Leu
+    'RUB', // Russian Ruble
+    'RWF', // Rwandan Franc
+    'SHP', // Saint Helena Pound
+    'WST', // Samoan Tala
+    'STN', // São Tomé and Príncipe Dobra
+    'SAR', // Saudi Riyal
+    'RSD', // Serbian Dinar
+    'SCR', // Seychellois Rupee
+    'SLL', // Sierra Leonean Leone
+    'SGD', // Singapore Dollar
+    'SBD', // Solomon Islands Dollar
+    'SOS', // Somali Shilling
+    'ZAR', // South African Rand
+    'KRW', // South Korean Won
+    'SSP', // South Sudanese Pound
+    'LKR', // Sri Lankan Rupee
+    'SDG', // Sudanese Pound
+    'SRD', // Surinamese Dollar
+    'SZL', // Swazi Lilangeni
+    'SEK', // Swedish Krona
+    'CHF', // Swiss Franc
+    'SYP', // Syrian Pound
+    'TWD', // New Taiwan Dollar
+    'TJS', // Tajikistani Somoni
+    'TZS', // Tanzanian Shilling
+    'THB', // Thai Baht
+    'TOP', // Tongan Paʻanga
+    'TTD', // Trinidad and Tobago Dollar
+    'TND', // Tunisian Dinar
+    'TRY', // Turkish Lira
+    'TMT', // Turkmenistani Manat
+    'UGX', // Ugandan Shilling
+    'UAH', // Ukrainian Hryvnia
+    'AED', // United Arab Emirates Dirham
+    'USD', // United States Dollar
+    'UYU', // Uruguayan Peso
+    'UZS', // Uzbekistani Som
+    'VUV', // Vanuatu Vatu
+    'VES', // Venezuelan Bolívar Soberano
+    'VND', // Vietnamese Đồng
+    'YER', // Yemeni Rial
+    'ZMW', // Zambian Kwacha
+    'ZWL', // Zimbabwean Dollar
+];
+
